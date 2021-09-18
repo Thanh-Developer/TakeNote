48d4e75f63851fa384432b70eb11b7b7deb537e7
Thanh <thanh.pham@v-key.com>
2021-09-18 10:15:30 +0700

    Init project

---
+++ app/src/main/java/com/demo/takenote/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.demo.takenote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.demo.takenote.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
