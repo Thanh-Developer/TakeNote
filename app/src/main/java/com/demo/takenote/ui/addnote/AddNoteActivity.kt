f60e9d304930e93af9024fcb64fec1ef42537cad
Thanh <thanh.pham@v-key.com>
2021-09-19 21:21:05 +0700

    Refactor viewmodel

--- app/src/main/java/com/demo/takenote/ui/addnote/AddNoteActivity.kt
+++ app/src/main/java/com/demo/takenote/ui/addnote/AddNoteActivity.kt
@@ -1,112 +1,93 @@
 package com.demo.takenote.ui.addnote
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.demo.takenote.R
 import com.demo.takenote.data.local.db.Note
 import com.demo.takenote.databinding.ActivityAddNoteBinding
 import com.demo.takenote.ui.home.HomeActivity
-import com.demo.takenote.utils.Coroutines
-import com.demo.takenote.utils.DateUtils.getCurrentDateTime
-import com.demo.takenote.utils.DateUtils.getFormattedDateString
 import com.demo.takenote.utils.UtilExtensions.hideKeyboard
 import com.demo.takenote.utils.UtilExtensions.myToast
-import com.demo.takenote.utils.UtilExtensions.setTextEditable
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class AddNoteActivity : AppCompatActivity(), View.OnClickListener {
+/**
+ *  Create by ThanhPQ
+ */
+class AddNoteActivity : AppCompatActivity() {
     private val viewModel: AddNoteViewModel by viewModel()
     private lateinit var binding: ActivityAddNoteBinding
-    private var note: Note? = null
+    private var noteData: Note? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAddNoteBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         initView()
         initData()
+        observeField()
+    }
+
+    private fun observeField() {
+        with(viewModel) {
+            isUpdateSuccess.observe(this@AddNoteActivity, {
+                if (it) {
+                    hideKeyboard(this@AddNoteActivity)
+                    myToast(getString(R.string.success_update))
+                    finish()
+                    overridePendingTransition(R.anim.stay, R.anim.slide_down)
+                }
+            })
+
+            isInsertSuccess.observe(this@AddNoteActivity, {
+                if (it) {
+                    hideKeyboard(this@AddNoteActivity)
+                    myToast(getString(R.string.success_save))
+                    finish()
+                    overridePendingTransition(R.anim.stay, R.anim.slide_down)
+                }
+            })
+
+            isValidateSuccess.observe(this@AddNoteActivity, {
+                if (!it) {
+                    myToast(getString(R.string.form_empty))
+                }
+            })
+
+            isDeleteSuccess.observe(this@AddNoteActivity, {
+                hideKeyboard(this@AddNoteActivity)
+                if (it) {
+                    myToast(getString(R.string.success_delete))
+                }
+                finish()
+                overridePendingTransition(R.anim.stay, R.anim.slide_down)
+            })
+        }
     }
 
     private fun initView() {
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         binding.apply {
-            toolBar.apply {
-                btnClose.setOnClickListener(this@AddNoteActivity)
-                btnDone.setOnClickListener(this@AddNoteActivity)
-            }
-            txtTime.text = getFormattedDateString(getCurrentDateTime)
+            viewModel = this@AddNoteActivity.viewModel
+            lifecycleOwner = this@AddNoteActivity
+            executePendingBindings()
         }
     }
 
     private fun initData() {
-        note = intent.extras?.getParcelable(HomeActivity.NOTE_DATA)
-        if (note != null) {
-            binding.apply {
-                edtTitle.setTextEditable(note?.title ?: "")
-                edtDesc.setTextEditable(note?.description ?: "")
-                txtTime.text = note?.date ?: ""
+        noteData = intent.extras?.getParcelable(HomeActivity.NOTE_DATA)
 
-                toolBar.apply {
-                    title.text = getString(R.string.update_note)
-                    btnClose.setImageResource(R.drawable.ic_delete)
-                }
+        if (noteData != null) {
+            viewModel.apply {
+                note.value = noteData
+                title.value = noteData?.title ?: ""
+                des.value = noteData?.description ?: ""
+                date.value = noteData?.date ?: ""
+            }
+
+            binding.toolBar.apply {
+                title.text = getString(R.string.update_note)
+                btnClose.setImageResource(R.drawable.ic_delete)
             }
         }
-    }
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.btnClose -> handleClose()
-            R.id.btnDone -> handleDone()
-        }
-    }
-
-    private fun handleDone() {
-        hideKeyboard(this)
-
-        val id = if (note != null) note?.id else null
-        val title = binding.edtTitle.text.toString().trim()
-        val desc = binding.edtDesc.text.toString().trim()
-        val date = getFormattedDateString(getCurrentDateTime)
-        val priority = 1
-
-        if (title.isEmpty() || desc.isEmpty()) {
-            myToast(getString(R.string.form_empty))
-            return
-        }
-
-        val note =
-            Note(id = id, title = title, description = desc, priority = priority, date = date)
-        Coroutines.main {
-            if (id != null) {
-                //for update note
-                viewModel.updateNote(note).also {
-                    myToast(getString(R.string.success_update))
-                }
-            } else {
-                //for insert note
-                viewModel.insertNote(note).also {
-                    myToast(getString(R.string.success_save))
-                }
-            }
-        }
-
-        finish()
-        overridePendingTransition(R.anim.stay, R.anim.slide_down)
-    }
-
-    private fun handleClose() {
-        hideKeyboard(this)
-        val id = if (note != null) note?.id else null
-        Coroutines.main {
-            if (id != null) {
-                viewModel.deleteNoteById(id).apply {
-                    myToast(getString(R.string.success_delete))
-                }
-            }
-        }
-        finish()
-        overridePendingTransition(R.anim.stay, R.anim.slide_down)
     }
 
 }
--- app/src/main/java/com/demo/takenote/ui/addnote/AddNoteViewModel.kt
+++ app/src/main/java/com/demo/takenote/ui/addnote/AddNoteViewModel.kt
@@ -1,23 +1,73 @@
 package com.demo.takenote.ui.addnote
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.demo.takenote.data.local.db.Note
 import com.demo.takenote.data.repository.NoteRepository
+import com.demo.takenote.utils.Coroutines
+import com.demo.takenote.utils.DateUtils.getCurrentDateTime
+import com.demo.takenote.utils.DateUtils.getFormattedDateString
 
 /**
  *  Create by ThanhPQ
  */
 class AddNoteViewModel(private val repository: NoteRepository) : ViewModel() {
 
-    suspend fun insertNote(note: Note) = repository.insertNote(note)
+    private suspend fun insertNote(note: Note) = repository.insertNote(note)
 
-    suspend fun updateNote(note: Note) = repository.updateNote(note)
+    private suspend fun updateNote(note: Note) = repository.updateNote(note)
 
-    suspend fun deleteNote(note: Note) = repository.deleteNote(note)
+    private suspend fun deleteNoteById(id: Int) = repository.deleteNoteById(id)
 
-    suspend fun deleteNoteById(id: Int) = repository.deleteNoteById(id)
+    var note = MutableLiveData<Note>().apply { value = null }
+    var title = MutableLiveData<String>().apply { value = "" }
+    var des = MutableLiveData<String>().apply { value = "" }
+    var date = MutableLiveData<String>().apply {
+        value = getFormattedDateString(getCurrentDateTime)
+    }
+    var isUpdateSuccess = MutableLiveData<Boolean>()
+    var isInsertSuccess = MutableLiveData<Boolean>()
+    var isValidateSuccess = MutableLiveData<Boolean>()
+    var isDeleteSuccess = MutableLiveData<Boolean>()
 
-    suspend fun clearNote() = repository.clearNote()
+    fun onSaveNote() {
+        val id = note.value?.id
+        val titleNote = title.value.toString().trim()
+        val desc = des.value.toString().trim()
+        val date = getFormattedDateString(getCurrentDateTime)
+        val priority = 1
 
-    fun getAllNotes() = repository.getAllNotes()
+        if (titleNote.isEmpty() || desc.isEmpty()) {
+            isValidateSuccess.value = false
+            return
+        }
+
+        val note = Note(id, titleNote, desc, priority, date)
+        Coroutines.main {
+            if (id != null) {
+                //for update note
+                updateNote(note).also {
+                    isUpdateSuccess.value = true
+                }
+            } else {
+                //for insert note
+                insertNote(note).also {
+                    isInsertSuccess.value = true
+                }
+            }
+        }
+    }
+
+    fun onCloseOrDelete() {
+        val id = note.value?.id
+        Coroutines.main {
+            if (id != null) {
+                deleteNoteById(id).apply {
+                    isDeleteSuccess.value = true
+                }
+            }
+        }
+        isDeleteSuccess.value = false
+    }
+
 }
